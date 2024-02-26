5495cbbc35d5919daff0cd7e28dc9637d5641af9
Soham Patil <sohamppatil7@gmail.com>
2024-02-26 09:16:30 -0500

    v1 - st added

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st 
+import main
+import os
+import base64
+
+cohere_api_key = 'U4YiqSDHIkdFIItNWEwLVA7nLQ1aQm2HCyUmdu6h'
+
+st.set_page_config(layout="wide")
+st.markdown("<h1 style='text-align: center; font-size:70px;'> सहायकAI 📃\n</h1>", unsafe_allow_html=True)
+st.text("")
+st.text("")
+col1, col2 = st.columns(2)
+
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:  # This is the central column
+    uploaded_file = st.file_uploader("Choose PDF:", type="pdf")
+
+# Proceed with the rest of the layout only if a file has been uploaded
+if uploaded_file is not None:
+    cohere_api_key = 'U4YiqSDHIkdFIItNWEwLVA7nLQ1aQm2HCyUmdu6h'
+
+    # Display the original PDF in the left column
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("अंग्रेजी में")
+        # To display the PDF we will use a little trick with base64 encoding and an iframe
+        st.write("आपका दस्तावेज़:")
+        base64_pdf = base64.b64encode(uploaded_file.getvalue()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+    # Display the translated PDF in the right column
+    with col2:
+        st.header("हिंदी में")
+        with st.spinner("आपके दस्तावेज़ का अनुवाद हो रहा है......"):
+            with open("temp_file.pdf", "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            translated_pdf_path = main.process_pdf("temp_file.pdf", cohere_api_key)
+        
+        if os.path.exists(translated_pdf_path):
+            with open(translated_pdf_path, "rb") as f:
+                st.write("अनुवादित दस्तावेज़:")
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
+                st.markdown(pdf_display, unsafe_allow_html=True)
+                f.seek(0)
+                st.download_button("Download Translated PDF", f.read(), file_name="translated_output.pdf")
+
+        # Clean up the temp file after use
+        os.remove("temp_file.pdf")
+
+# with col1:
+#     st.header("अंग्रेजी दस्तावेज़")
+#     uploaded_file = st.file_uploader("Choose PDF:", type="pdf")
+#     if uploaded_file is not None:
+#         st.write("Original document:")
+#         base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
+#         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+#         st.markdown(pdf_display, unsafe_allow_html=True)
+        
+# with col2:
+#     st.header("हिंदी दस्तावेज़")
+#     if uploaded_file is not None and cohere_api_key:
+#         with st.spinner("Translating your document..."):
+#             translated_pdf_path = main.process_pdf("temp_file.pdf", cohere_api_key)
+#         if os.path.exists(translated_pdf_path):
+#             st.write("Translated document:")
+#             with open(translated_pdf_path, "rb") as f:
+#                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#                 pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+#                 st.markdown(pdf_display, unsafe_allow_html=True)
+#                 st.download_button("Download Translated PDF", f.read(), file_name="translated_output.pdf")
+        
---
+++ main.py
@@ -0,0 +1,61 @@
+import cohere
+import fitz
+from fpdf import FPDF
+
+cohere_api = 'U4YiqSDHIkdFIItNWEwLVA7nLQ1aQm2HCyUmdu6h'
+
+class PDF(FPDF):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_font('Hindi', '', 'AnnapurnaSIL-Regular.ttf', uni=True)
+
+    def header(self):
+        self.set_font('Arial','I',10)
+        self.cell(0, 10, 'Translated using SahayakAI', 0, 1, 'C')
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+def extract_text(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    doc.close()
+    return text
+
+def aya_translation(input_text, cohere_api_key):
+    co = cohere.Client(cohere_api)  # This is your trial API key
+    fix_prompt = 'Translate and finish this letter for me in Hindi while maintaining the format in output text:'
+    end_prompt = 'Ensure your output translation is in Hindi language'
+    response = co.generate(
+        model='c4ai-aya',
+        prompt=fix_prompt + input_text + end_prompt,
+        max_tokens=3000,
+        temperature=0.9,
+        k=0,
+        stop_sequences=[],
+        return_likelihoods='NONE')
+    return response.generations[0].text
+
+def save_text_to_pdf(text, filename):
+    pdf = PDF()
+    pdf.add_page()
+    pdf.set_font('Hindi', '', 14)
+    pdf.set_auto_page_break(auto=True, margin=5)  # Set auto page break
+
+    # Split the text into lines
+    lines = text.split('\n')
+    for line in lines:
+        pdf.cell(0, 10, line, ln=1, align='L')  # Use cell with ln=1 for automatic new lines
+
+    pdf.output(filename)
+
+def process_pdf(file_path, cohere_api_key):
+    text = extract_text(file_path)
+    hindi_translation = aya_translation(text, cohere_api_key)
+    output_pdf_path = 'translated_output.pdf'
+    save_text_to_pdf(hindi_translation, output_pdf_path)
+    return output_pdf_path
